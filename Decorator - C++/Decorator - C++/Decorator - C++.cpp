2e1535401355d47fc7e8628c3f6395a2a2ec85a3
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-20 20:04:21 +0200

    Add files via upload

---
+++ Decorator - C++/Decorator - C++/Decorator - C++.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+__interface IProcessor {
+	virtual void Process() = 0;
+};
+
+class Transmitter : public IProcessor {
+private:
+	string data;
+public:
+	Transmitter(string d) : data(d){}
+	void Process() override {
+		cout << "Data" << data << "beamed" << "\n";
+	}
+};
+
+class Shell : public IProcessor {
+protected:
+	IProcessor* processor;
+public:
+	Shell(IProcessor* pr) : processor(pr){}
+	void Process() override {
+		processor->Process();
+	}
+};
+
+class HammingCoder : public Shell {
+public:
+	HammingCoder(IProcessor* pr) : Shell(pr){}
+	void Process() override {
+		cout << "Hamming's noise-resistant code is imposed->";
+		processor->Process();
+	}
+};
+
+class Encryptor : public Shell {
+public:
+	Encryptor(IProcessor* pr) : Shell(pr){}
+	void Process() override {
+		cout << "Data encryption->";
+		processor->Process();
+	}
+};
+
+int main() {
+	IProcessor* transmitter = new Transmitter("1234");
+	transmitter->Process();
+	cout << "\n";
+
+	Shell* hammingCode = new HammingCoder(transmitter);
+	hammingCode->Process();
+	cout << "\n";
+
+	Shell* encriptor = new Encryptor(hammingCode);
+	encriptor->Process();
+	cout << "\n";
+}
